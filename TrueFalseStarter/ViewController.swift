ae6935e386f7ff5800d74b006791dc84209136dc
Kirill Druzhynin <k.druzhynin@gmail.com>
2018-02-01 12:00:48 +0200

    Auto changes from Swift 3 to Swift 4

--- TrueFalseStarter/ViewController.swift
+++ TrueFalseStarter/ViewController.swift
@@ -12,14 +12,14 @@ import AudioToolbox
 
 class ViewController: UIViewController {
     
-    let questionsPerRound = 4
-    var questionsAsked = 0
-    var correctQuestions = 0
-    var indexOfSelectedQuestion: Int = 0
+    @objc let questionsPerRound = 4
+    @objc var questionsAsked = 0
+    @objc var correctQuestions = 0
+    @objc var indexOfSelectedQuestion: Int = 0
     
-    var gameSound: SystemSoundID = 0
+    @objc var gameSound: SystemSoundID = 0
     
-    let trivia: [[String : String]] = [
+    @objc let trivia: [[String : String]] = [
         ["Question": "Only female koalas can whistle", "Answer": "False"],
         ["Question": "Blue whales are technically whales", "Answer": "True"],
         ["Question": "Camels are cannibalistic", "Answer": "False"],
@@ -45,14 +45,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func displayQuestion() {
+    @objc func displayQuestion() {
         indexOfSelectedQuestion = GKRandomSource.sharedRandom().nextInt(upperBound: trivia.count)
         let questionDictionary = trivia[indexOfSelectedQuestion]
         questionField.text = questionDictionary["Question"]
         playAgainButton.isHidden = true
     }
     
-    func displayScore() {
+    @objc func displayScore() {
         // Hide the answer buttons
         trueButton.isHidden = true
         falseButton.isHidden = true
@@ -81,7 +81,7 @@ class ViewController: UIViewController {
         loadNextRoundWithDelay(seconds: 2)
     }
     
-    func nextRound() {
+    @objc func nextRound() {
         if questionsAsked == questionsPerRound {
             // Game is over
             displayScore()
@@ -105,7 +105,7 @@ class ViewController: UIViewController {
     
     // MARK: Helper Methods
     
-    func loadNextRoundWithDelay(seconds: Int) {
+    @objc func loadNextRoundWithDelay(seconds: Int) {
         // Converts a delay in seconds to nanoseconds as signed 64 bit integer
         let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
         // Calculates a time value to execute the method given current time and delay
@@ -117,13 +117,13 @@ class ViewController: UIViewController {
         }
     }
     
-    func loadGameStartSound() {
+    @objc func loadGameStartSound() {
         let pathToSoundFile = Bundle.main.path(forResource: "GameSound", ofType: "wav")
         let soundURL = URL(fileURLWithPath: pathToSoundFile!)
         AudioServicesCreateSystemSoundID(soundURL as CFURL, &gameSound)
     }
     
-    func playGameStartSound() {
+    @objc func playGameStartSound() {
         AudioServicesPlaySystemSound(gameSound)
     }
 }
